1856840238f46aedf0847254dddb5724778a452e
Daniel Leussa <leussadaniel04@gmail.com>
2023-05-15 22:03:53 +0100

    making Models & API resources by using artisan

---
+++ backend/Api/app/Http/Controllers/CommentLikeController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\CommentLike;
+use Illuminate\Http\Request;
+
+class CommentLikeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(CommentLike $commentLike)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, CommentLike $commentLike)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(CommentLike $commentLike)
+    {
+        //
+    }
+}
---
+++ backend/Api/app/Http/Controllers/FollowController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Follow;
+use Illuminate\Http\Request;
+
+class FollowController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Follow $follow)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Follow $follow)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Follow $follow)
+    {
+        //
+    }
+}
